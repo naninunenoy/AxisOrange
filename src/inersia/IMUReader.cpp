c0b2b458a68e849f0f3ca3a9486db4bb7a8e1314
nakano yosuke <naninuneno0411@gmail.com>
2019-07-25 01:12:47 +0900

    fix code to adapt m5stickc 0.0.7

--- src/inersia/IMUReader.cpp
+++ src/inersia/IMUReader.cpp
@@ -13,12 +13,12 @@ namespace inersia {
         // get sensor value
         IMU* tmp = ((IMU*)imu);
         // acc
-        tmp->getAccelData(&sensorRawAcc[0], &sensorRawAcc[1], &sensorRawAcc[2]);
+        tmp->getAccelAdc(&sensorRawAcc[0], &sensorRawAcc[1], &sensorRawAcc[2]);
         for (int i = 0; i < 3; i++) {
             sensorAcc[i] = sensorRawAcc[i] * tmp->aRes;
         }
         // gyro
-        tmp->getGyroData(&sensorRawGyro[0], &sensorRawGyro[1], &sensorRawGyro[2]);
+        tmp->getGyroAdc(&sensorRawGyro[0], &sensorRawGyro[1], &sensorRawGyro[2]);
         for (int i = 0; i < 3; i++) {
             sensorGyro[i] = sensorRawGyro[i] * tmp->gRes;
         }
