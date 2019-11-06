2e3fc4fb409e6d6950fb4824a9987a4a7d7e5f3a
nakano yosuke <naninuneno0411@gmail.com>
2019-11-06 23:32:39 +0900

    header comment

--- src/mahony/MahonyAHRS.cpp
+++ src/mahony/MahonyAHRS.cpp
@@ -1,3 +1,17 @@
+//=====================================================================================================
+// MahonyAHRS.c
+//=====================================================================================================
+//
+// Madgwick's implementation of Mayhony's AHRS algorithm.
+// See: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
+//
+// Date			Author			Notes
+// 29/09/2011	SOH Madgwick    Initial release
+// 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
+//
+//=====================================================================================================
+// from https://github.com/m5stack/M5StickC/blob/master/src/utility/MahonyAHRS.cpp
+
 #include <math.h>
 #include <M5StickC.h>
 #include "MahonyAHRS.h"
